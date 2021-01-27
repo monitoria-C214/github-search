01bc23ae9004ce8b3c6e94a6d73d1dcb2273c718
Vanessa Swerts <vanessaswerts@gec.inatel.br>
2021-01-27 15:47:33 -0300

    alterando testes unitários da Home

--- src/__tests__/unit/pages/Home.test.js
+++ src/__tests__/unit/pages/Home.test.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Home } from '../../../pages'
-import { fireEvent, render, screen, waitFor } from '@testing-library/react'
+import { fireEvent, render, screen } from '@testing-library/react'
 
 describe('Teste Unitário da Home', () => {
 
@@ -11,22 +11,62 @@ describe('Teste Unitário da Home', () => {
     expect(input.value).toBe('')
   })
 
-  it("Deve alterar o input value para VanessaSwerts", () => {
+  it("Deve alterar o valor do input para VanessaSwerts", () => {
     render(<Home />)
 
     const input = screen.getByPlaceholderText("Github username...")
-    fireEvent.change(input, {target: {value: 'VanessaSwerts'}})
+    fireEvent.change(input, { target: { value: 'VanessaSwerts' } })
 
     expect(input.value).toBe('VanessaSwerts')
   })
-  
-  // it("Ao abrir o site, renderiza mensagem 'Carregando...' ", async () => {
-  //   render(<Home />)
 
-  //   const loading = await screen.findByText('Carregando ...')
-  //   expect(loading).toBeInTheDocument()
-  //   // expect(loading.textContent).toEqual('Carregando ...')
-  //   // expect(loading).toHaveTextContent('Carregando ...')
-  // })
+  it("Quando abrir o site, deve renderizar o card examplo", () => {
+    const { getByTestId } = render(<Home />)
+
+    const card = getByTestId('card-user')
+    expect(card).toBeInTheDocument()
+  })
+
+  it("Quando abrir o site, deve renderizar o card examplo com o dados corretos", () => {
+    render(<Home />)
+
+    const user = {
+      name: "Example",
+      username: "username",
+      location: "location",
+      followers: "0",
+      following: "0",
+      repos: "0",
+      avatar: 'https://avatars.githubusercontent.com/u/57936?v=4'
+    }
+
+    const avatar = screen.getByTestId('card-avatar')
+    const name = screen.getByTestId('card-name')
+    const username = screen.getByTestId('card-username')
+    const location = screen.getByTestId('card-location')
+    const following = screen.getByTestId('card-following')
+    const followers = screen.getByTestId('card-followers')
+    const repos = screen.getByTestId('card-repos')
+
+    const userTest = {
+      name: name.textContent,
+      username: username.textContent,
+      location: location.textContent,
+      followers: following.textContent,
+      following: followers.textContent,
+      repos: repos.textContent,
+      avatar: avatar.src
+    }
+
+    expect(userTest).toEqual(user)
+
+    // expect(avatar).toHaveAttribute("src", "https://avatars.githubusercontent.com/u/57936?v=4")
+    // expect(name.textContent).toBe("Example")
+    // expect(username).toHaveTextContent('username')
+    // expect(location).toHaveTextContent('location')
+    // expect(following).toHaveTextContent('0')
+    // expect(followers).toHaveTextContent('0')
+    // expect(repos).toHaveTextContent('0')
+  })
 
 })
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -15,7 +15,7 @@ export default function Home() {
     following: "0",
     repos: "0",
     avatar: 'https://avatars.githubusercontent.com/u/57936?v=4'
-  })  
+  })
 
   const handleSetData = ({ name, login, followers, following, public_repos, avatar_url, location }) => {
     setData({
@@ -72,31 +72,36 @@ export default function Home() {
             <div data-testid="card-user" className="card-user">
               <div className="card-image">
                 <img
+                  data-testid="card-avatar"
                   src={data.avatar}
                   alt="User Avatar"
                 />
               </div>
 
               <div className="card-content">
-                {data.name && <h1 className="card-name">{data.name}</h1>}
+                {data.name && <h1 data-testid="card-name" className="card-name">{data.name}</h1>}
                 <span className="card-username">
                   <i className="fas fa-user" />
                   <span data-testid="card-username">{data.username}</span>
                 </span>
                 {data.location && <span className="card-location">
-                  <i className="fas fa-map-marker-alt" />{data.location}
+                  <i className="fas fa-map-marker-alt" />
+                  <span data-testid="card-location">{data.location}</span>
                 </span>}
 
                 <div className="card-data">
                   <span>
-                    <i className="fas fa-user-friends" />{data.following} following
-              </span>
+                    <i className="fas fa-user-friends" />
+                    <span data-testid="card-following" >{data.following}</span> following
+                  </span>
                   <span>
-                    <i className="fas fa-user-friends" />{data.followers} followers
-              </span>
+                    <i className="fas fa-user-friends" />
+                    <span data-testid="card-followers">{data.followers}</span> followers
+                  </span>
                   <span>
-                    <i className="fas fa-poll-h" />{data.repos} repos
-              </span>
+                    <i className="fas fa-poll-h" />
+                    <span data-testid="card-repos">{data.repos}</span> repos
+                  </span>
                 </div>
 
                 <div className="card-footer">
