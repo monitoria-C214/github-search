aa5a325bd3653a9d2f4c9fc8d361583ae8786998
Vanessa Swerts <vanessaswerts@gec.inatel.br>
2021-01-31 15:21:18 -0300

    Atualizando testes unitários da home

--- src/__tests__/unit/pages/Home.test.js
+++ src/__tests__/unit/pages/Home.test.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Home } from '../../../pages'
-import { fireEvent, render, screen } from '@testing-library/react'
-import userEvent  from '@testing-library/user-event'
+import { fireEvent, render, screen, waitFor, act } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+
 
 // Vamos mockar a nossa API
-import api from '../../../services/api'
+import apiMock from '../../../services/api'
 jest.mock('../../../services/api')
 
+jest.useFakeTimers()
 
 describe('Teste Unitário da Home', () => {
 
@@ -14,7 +16,7 @@ describe('Teste Unitário da Home', () => {
     render(<Home />)
 
     const input = screen.getByTestId('search-input')
-    
+
     // expect(input.value).toBe('')
     expect(input).toHaveValue('')
   })
@@ -57,8 +59,8 @@ describe('Teste Unitário da Home', () => {
       name: name.textContent,
       username: username.textContent,
       location: location.textContent,
-      followers: following.textContent,
-      following: followers.textContent,
+      followers: followers.textContent,
+      following: following.textContent,
       repos: repos.textContent,
       avatar: avatar.src
     }
@@ -79,7 +81,7 @@ describe('Teste Unitário da Home', () => {
   it("Deve alterar o valor do input para VanessaSwerts", () => {
     render(<Home />)
 
-    const input = screen.getByPlaceholderText("Github username...") 
+    const input = screen.getByPlaceholderText("Github username...")
 
     //Podemos simular a mudança do value do Input usando o fireEvent
     // fireEvent.change(input, { target: { value: 'VanessaSwerts' } })
@@ -91,63 +93,108 @@ describe('Teste Unitário da Home', () => {
     // expect(input.value).toBe('VanessaSwerts')
   })
 
-  it('Ao pesquisar novo usuario, primeiramente mostra o Carregando ...', () => {
-    render(<Home />)
-    
-    // Objeto que esperamos retornar na promise
-    const userSearch = {
-      name: "Vanessa Swerts",
-      username: "VanessaSwerts",
-      location: "Minas Gerais - Brazil",
-      followers: "66",
-      following: "51",
-      repos: "19",
-      avatar: 'https://avatars.githubusercontent.com/u/57146734?v=4'
-    }
-
-    // Chamamos o api mock e simulamos o retorno do objeto
-    api.get.mockResolvedValue({ data: userSearch })
-    
-    // Simulamos que o usuário pesquisando um novo o username 
-    const input = screen.getByPlaceholderText("Github username...")
-    userEvent.type(input, "VanessaSwerts")
-
-    const form = screen.getByTestId("search-form")
-    fireEvent.submit(form) 
-
-    // Pegamos o componente responsável por mostrar a mensagem Carregando
-    const loading = screen.getByTestId('message-loading') 
-
-    // Verificamos se ela realmente aparece na tela.
-    // expect(loading).toHaveTextContent('Carregando...')
-    expect(loading).toBeInTheDocument()
-  })
-
-  // it('Após pesquisar, deve mostrar o dados do usuário pesquisado!', async () => {
+  // it('Ao pesquisar novo usuario, primeiramente mostra o Carregando ...', () => {
   //   render(<Home />)
 
+  //   // Objeto que esperamos retornar na promise
   //   const userSearch = {
   //     name: "Vanessa Swerts",
   //     username: "VanessaSwerts",
   //     location: "Minas Gerais - Brazil",
-  //     followers: "66",
-  //     following: "51",
+  //     followers: "51",
+  //     following: "66",
   //     repos: "19",
   //     avatar: 'https://avatars.githubusercontent.com/u/57146734?v=4'
   //   }
 
-  //   api.get.mockResolvedValue({ data: userSearch })
-    
+  //   // Chamamos o api mock e simulamos o retorno do objeto
+  //   apiMock.get.mockResolvedValue({ data: userSearch })
+
+  //   // Simulamos que o usuário pesquisando um novo o username 
   //   const input = screen.getByPlaceholderText("Github username...")
-  //   fireEvent.change(input, { target: { value: 'VanessaSwerts' } })
+  //   userEvent.type(input, "VanessaSwerts")
 
   //   const form = screen.getByTestId("search-form")
-  //   fireEvent.submit(form) 
+  //   fireEvent.submit(form)
 
-  //   const card = await screen.findAllByTestId('card-user')
+  //   // Pegamos o componente responsável por mostrar a mensagem Carregando
+  //   const loading = screen.getByTestId('message-loading')
 
-  //   expect(api.get).toHaveBeenCalledTimes(1)
+  //   // Verificamos se ela realmente aparece na tela.
+  //   expect(loading).toHaveTextContent('Carregando...')
+  //   // expect(loading).toBeInTheDocument()
   // })
 
+  it('Após pesquisar, deve mostrar o dados do usuário pesquisado!', async () => {
+    render(<Home />)
+
+    const userSearch = {
+      name: "Vanessa Swerts",
+      // username: "VanessaSwerts",
+      location: "Minas Gerais - Brazil",
+      followers: "51",
+      following: "66",
+      // repos: "19",
+      // avatar: 'https://avatars.githubusercontent.com/u/57146734?v=4'
+    }
+
+    apiMock.get.mockResolvedValue({ data: userSearch })
+
+    // Simulando a pesquisa
+    const input = screen.getByPlaceholderText("Github username...")
+    fireEvent.change(input, { target: { value: 'VanessaSwerts' } })
+
+    const form = screen.getByTestId("search-form")
+    fireEvent.submit(form)
+
+    //Esperamos ele encontrar o componente do card
+    // await waitFor(() => expect(apiMock.get).toHaveBeenCalledTimes(1))
+    await screen.findByTestId('card-user')   
+   
+    // Buscando os valores dos componentes do card
+    const name = screen.getByTestId('card-name')
+    // const username = screen.getByTestId('card-username')
+    const location = screen.getByTestId('card-location')
+    const following = screen.getByTestId('card-following')
+    const followers = screen.getByTestId('card-followers')
+    // const repos = screen.getByTestId('card-repos')
+    // const avatar = screen.getByTestId('card-avatar')
+
+    // Criando objeto com os valores do componente
+    const userSearchTest = {
+      name: name.textContent,
+      // username: username.textContent,
+      location: location.textContent,
+      followers: followers.textContent,
+      following: following.textContent,
+      // repos: repos.textContent,
+      // avatar: avatar.src
+    }
+
+    // Conferindo se os valores do componente são iguais ao do retorno da apiMock
+    expect(userSearchTest).toEqual(userSearch)
+  })
+
+  it('Ao pesquisar um usuario inválido, mostra a mensagem de erro ...', async () => {
+    render(<Home />)
+
+    apiMock.get.mockRejectedValue(new Error())
+
+    // Simulamos que o usuário pesquisando um novo o username 
+    const input = screen.getByPlaceholderText("Github username...")
+    userEvent.type(input, "vanessaswerts12345")
+
+    const form = screen.getByTestId("search-form")
+    fireEvent.submit(form)
+
+    // Esperamos realizar a requisição
+    await waitFor(() => expect(apiMock.get).toHaveBeenCalledTimes(1))
+   
+    // Pegamos o componente responsável por mostrar a mensagem de erro
+    const error = screen.getByTestId('message-error')
+
+    // Verificamos se ela realmente aparece na tela.
+    expect(error).toHaveTextContent('Ops, usuário não encontrado!')
+  })
 
 })
